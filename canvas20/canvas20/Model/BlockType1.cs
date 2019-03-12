50c8c7ab6781a1e58ccd6f51c6653a43e7256a7d
Andrei_Baravik <baravik.andrei@gmail.com>
2019-03-12 23:29:28 +0300

    last version

---
+++ canvas20/canvas20/Model/BlockType1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace canvas20
+{
+    public class BlockType1 : Box
+    {
+        private decimal someField1;
+        private decimal someField2;
+        private decimal multiplay;
+        private Visibility multivisibility;
+        private bool isMultiplay = false;
+
+        public bool IsMultiplay
+        {
+            get { return isMultiplay; }
+            set
+            {
+                isMultiplay = value;
+                OnPropertyChanged(nameof(IsMultiplay));
+            }
+        }
+        public Visibility MultiVisibility
+        {
+            get { return multivisibility; }
+            set
+            {
+                multivisibility = value;
+                OnPropertyChanged(nameof(MultiVisibility));
+            }
+        }
+        public decimal Multiplay
+        {
+            get { return multiplay; }
+            set
+            {
+                multiplay = value;
+                OnPropertyChanged(nameof(Multiplay));
+            }
+        }
+
+        public decimal SomeField1
+        {
+            get => someField1;
+            set
+            {
+                someField1 = value;
+                OnPropertyChanged();
+            }
+
+
+        }
+        public decimal SomeField2
+        {
+            get => someField2;
+            set
+            {
+                someField2 = value;
+                OnPropertyChanged();
+            }
+
+
+        }
+        public void Multiplying()
+        {
+            Multiplay = SomeField1 * SomeField2;
+        }
+    }
+}
